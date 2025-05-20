7327baba3d355b78bb692951c47d915f38650f41
KINO04666 <820349335@qq.com>
2025-05-20 20:08:15 +0800

    新增redis使用 由于大量数据在高并发下会导致数据库宕机 必须采用redis缓存解决 而大量数据主要的就是单词

--- 背单词/背单词_backend/project/src/main/java/com/word_learn/service/impl/WordServiceImpl.java
+++ 背单词/背单词_backend/project/src/main/java/com/word_learn/service/impl/WordServiceImpl.java
@@ -1,5 +1,8 @@
 package com.word_learn.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.word_learn.dto.PageRequest;
@@ -10,17 +13,22 @@ import com.word_learn.entity.Word;
 import com.word_learn.mapper.WordMapper;
 import com.word_learn.service.WordService;
 import com.word_learn.utils.BaseContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Slf4j
 @Service
 public class WordServiceImpl implements WordService {
     @Autowired
     private WordMapper wordMapper;
-
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
     @Override
     public List<WordRequest> getFavoriteWords(Long userId) {
         return wordMapper.findByUserId(userId);  // 获取用户收藏的单词
@@ -47,7 +55,19 @@ public class WordServiceImpl implements WordService {
     public List<WordRequest> getWords(PageRequest pageRequest) {
         try {
 //            PageHelper.startPage(pageRequest.getPage(), pageRequest.getSize());
-            List<WordRequest> wordRequests = wordMapper.selectAllWords(pageRequest.getType());
+            //修改一下逻辑 现在先尝试去redis查找 再去mysql找避免大量查询的情况
+            List<WordRequest> wordRequests;
+            String s = stringRedisTemplate.opsForValue().get("words" + pageRequest.getType());
+            if(s==null||s.isEmpty()){
+                log.info("从数据库存入redis中");
+                wordRequests = wordMapper.selectAllWords(pageRequest.getType());
+                s = objectMapper.writeValueAsString(wordRequests);
+                stringRedisTemplate.opsForValue().set("words"+pageRequest.getType(),s);
+            }else{
+                log.info("直接从redis获取");
+                wordRequests = objectMapper.readValue(s, new TypeReference<List<WordRequest>>() {
+                });
+            }
 //            PageInfo pageInfo = new PageInfo(wordRequests);
 //            PageResponse build = PageResponse.builder()
 //                    .pageNum(pageInfo.getPageNum())
@@ -56,7 +76,9 @@ public class WordServiceImpl implements WordService {
 //                    .total(pageInfo.getSize())
 //                    .build();
             return wordRequests;
-        }finally{
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } finally{
             PageHelper.clearPage();
         }
     }
--- 背单词/背单词_backend/project/src/test/java/com/word_learn/TestApplication.java
+++ 背单词/背单词_backend/project/src/test/java/com/word_learn/TestApplication.java
@@ -2,10 +2,7 @@ package com.word_learn;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+
 import com.word_learn.dto.HttpTranslateEntity;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
@@ -21,10 +18,10 @@ import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import org.springframework.ai.chat.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -32,47 +29,7 @@ import java.nio.charset.StandardCharsets;
 @SpringBootTest
 public class TestApplication {
     @Autowired
-    ChatClient chatClient;
-    @Autowired
     ObjectMapper objectMapper;
-    JsonObject translate(String text){
-        CloseableHttpClient build = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://api.siliconflow.cn/v1/chat/completions");
-        httpPost.addHeader("Authorization","Bearer sk-zawiuqzwhrtkjnsyfncbnrsltdocnlixpdvpynnjsotjuodk");
-        httpPost.addHeader("Content-Type","application/json");
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("model","Qwen/Qwen3-32B");
-        JsonObject messages2 = new JsonObject();
-        messages2.addProperty("role","user");
-        messages2.addProperty("content","It is hardly necessary for me to cite all the evidence of the depressing state of literacy. These figures from the Department of Education are sufficient: 27 million Americans cannot read at all. and a further 35million read al a level that is less than sufficient to survive in our society.");
-        JsonObject messages3 = new JsonObject();
-        messages3.addProperty("role","system");
-        messages3.addProperty("content","You are a translator,You need to translate English to Chinese(If input language is English ) or  Chinese to English (If input language is Chinese ) "+"please respond in the format{text:....}");
-        JsonArray array = new JsonArray();
-        array.add(messages2);
-        array.add(messages3);
-        jsonObject.add("messages",array);
-        JsonObject response_foramt = new JsonObject();
-        response_foramt.addProperty("type","json_object");
-        jsonObject.add("response_format",response_foramt);
-        System.out.println(jsonObject.toString());
-        httpPost.setEntity(new StringEntity(jsonObject.toString(), StandardCharsets.UTF_8));
-        try {
-            CloseableHttpResponse execute = build.execute(httpPost);
-            build.close();
-            execute.close();
-            HttpEntity entity = execute.getEntity();
-            try {
-                String string = EntityUtils.toString(entity);
-                JsonElement jsonObject1 =  JsonParser.parseString(string);
-                return jsonObject1.getAsJsonObject().get("choices").getAsJsonArray().get(0).getAsJsonObject().get("message").getAsJsonObject().get("content").getAsJsonObject();
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     @Test
     void test2(){
         HttpTranslateEntity instance = HttpTranslateEntity.getInstance();
@@ -83,5 +40,12 @@ public class TestApplication {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @Test
+    void test3(){
+
+        stringRedisTemplate.opsForValue().set("kinoasdasd","123");
     }
 }
